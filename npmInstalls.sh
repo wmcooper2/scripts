881ebf27016a4e8ea6deeffd85c50899ad7dc2c3
wmc2tokyo <wmcooper2@yahoo.com>
2019-11-25 15:03:18 +0900

    Added simple script for setting up my react environment for new projects.

---
+++ npmInstalls.sh
@@ -0,0 +1,38 @@
+npm install --save jquery@3.4.1
+npm install --save popper.js@^1.14.7
+npm install --save bootstrap
+npm install --save typescript@>=2.8.0
+
+
+# REDUX
+npm install --save redux react-redux
+
+
+# MARKDOWN PREVIEWER
+# npm install --save marked
+# https://www.npmjs.com/package/marked
+
+
+# BABEL
+# npm install --save babel-cli babel-core --save-dev
+# npm install --save babel-preset-es2015 --save-dev
+# npm install --save @babel/plugin-transform-react-jsx
+# npx babel-upgrade
+
+# https://ccoenraets.github.io/es6-tutorial/setup-babel/
+# https://babeljs.io/docs/en/env/
+# https://babeljs.io/docs/en/babel-plugin-transform-react-jsx
+
+
+# FONT AWESOME
+# npm install --save react-icons
+# npm install --save font-awesome
+# npm install --save @fortawesome/fontawesome-svg-core
+# npm install --save @fortawesome/free-solid-svg-icons
+# npm install --save @fortawesome/free-brands-svg-icons
+# npm install --save @fortawesome/react-fontawesome
+
+
+# CLEANUP
+# npm audit
+npm audit fix --force
